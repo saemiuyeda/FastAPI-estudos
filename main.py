101232d09ab9bf102b63e664577d24f754ffa7ff
saemiuyeda <saemimarutani@gmail.com>
2026-04-14 21:25:41 -0300

    Adiciona modelo de entrada para o endpoint

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    text: str
+
 @app.post("/generate")
 def post_generate():
-
     return {
         "status": "ok"
     }
