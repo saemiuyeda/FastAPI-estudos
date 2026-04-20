5ed9696f6c52b3026873ff2cd2652632db23d82a
saemiuyeda <saemimarutani@gmail.com>
2026-04-19 23:15:24 -0300

    Adiciona função de geração de resposta com normalização de texto

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import unicodedata
 
 app = FastAPI()
 
 class Item(BaseModel):
     text: str
 
+def chatbot(message: str):
+    processed_message = ""
+
+    formatted_message = message.lower().strip()
+    formatted_and_normalized_message = unicodedata.normalize('NFD', formatted_message)
+    for char in formatted_and_normalized_message:
+        if not unicodedata.combining(char):
+            processed_message += char
+
+    if processed_message == "ola":
+        return "Olá! Como posso te ajudar?"
+    else:
+        return "Não entendi, poderia repetir?"
+
 @app.post("/generate")
-def post_generate(item: Item):
+async def post_generate(item: Item):
     item_data = item.model_dump()
     return item_data
