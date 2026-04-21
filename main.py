d740c2f0c387289be1ab6286e76afda69b0b3b82
saemiuyeda <saemimarutani@gmail.com>
2026-04-20 22:53:23 -0300

    refactor: extrai normalização de texto para função separada

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 class Item(BaseModel):
     text: str
 
-def chatbot(message: str):
+def normalize_text(message: str):
     processed_message = ""
 
     formatted_message = message.lower().strip()
@@ -15,13 +15,18 @@ def chatbot(message: str):
     for char in formatted_and_normalized_message:
         if not unicodedata.combining(char):
             processed_message += char
+    return processed_message
 
-    if processed_message == "ola":
-        return "Olá! Como posso te ajudar?"
+def generate_response(message: str):
+    if message == "olá":
+        return "Olá! Como posso ajudar?"
     else:
         return "Não entendi, poderia repetir?"
 
 @app.post("/generate")
 async def post_generate(item: Item):
-    item_data = item.model_dump()
-    return item_data
+    item_text = item.text
+
+    chatbot = generate_response(item_text)
+
+    return chatbot
